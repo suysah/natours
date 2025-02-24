7a125af00013a62d6dee21555926800398903bb2
Suyash Nagar <suyashnagar81@gmail.com>
2025-02-24 17:34:54 +0530

    errr to err

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,7 @@
 const AppError = require('./../utils/appError');
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errrmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   const message = `Duplicate field value: ${value}. Please use another value`;
   return new AppError(message, 400);
 };
